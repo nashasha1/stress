d0bf62614f0e35b665dcc04a9235ce15b6c8bc6d
root <root@ip-172-31-19-122.us-west-2.compute.internal>
2015-02-09 01:36:45 +0000

    limit cpu

--- cpustress.py
+++ cpustress.py
@@ -3,14 +3,16 @@ import time
 def dead_loop():
     a = time.time()
     print a
+    count = 0
     while True:
         b=time.time()
         if b - a > 10:
             print 'sleep 1s'
+            print a,'---',count
+            count = 0
             time.sleep(1)
             a=time.time()
-            print a
-        pass
+        count += 1
 if __name__ == '__main__':
     dead_loop()
     print time.time()
